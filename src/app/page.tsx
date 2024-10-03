3dafc7829811620d34bc2e9bb644444007698570
Elanchezhiyan Varadhanv <elanchezhiyan.varadhan@bounteous.com>
2024-10-03 11:53:53 +0530

    second change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and say hello.</li>
+          <li>Save and say bye.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
